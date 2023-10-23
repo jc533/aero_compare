3ab5d7b100f054b3cd781979cc6c4e264e7ab45f
JustinChen <yub612@gmail.com>
2023-10-23 19:23:08 +0800

    stupid select path

--- app/page.js
+++ app/page.js
@@ -1,18 +1,25 @@
+"use client"
 import Navbar from "./navbar.js"
 import IterSelect from "./iterSelect.js"
 import IterTable from "./iterTable.js"
 import IterImg from "./iterImg.js"
 import PathSelect from "./pathSelect.js"
+import { useState } from "react"
+import { createContext } from "react"
 
-
+export const PathContext = createContext(null)
 export default function Home() {
+	const [path,setPath] = useState("./")
+
 	return (
-		<main className="">
-			<Navbar/>
-			<PathSelect/>
-			<IterSelect/>
-			<IterImg/>
-			<IterTable/>
-		</main>
+		<PathContext.Provider value={path}>
+			<main className="">
+				<Navbar/>
+				<PathSelect setPath={setPath}/>
+				<IterSelect/>
+				<IterImg/>
+				<IterTable/>
+			</main>
+		</PathContext.Provider>
 	)
 }
--- app/pathSelect.js
+++ app/pathSelect.js
@@ -1,8 +1,31 @@
-import * as React from 'react'
+"use client"
+import Button from '@mui/material/Button';
+import { Stack, Typography } from '@mui/material';
+import { useContext } from 'react';
+import { PathContext } from './page';
 
 
-export default function PathSelect(){
+export default function PathSelect(props){
+    const path = useContext(PathContext)
+    const selectPath = () => {
+        let p = prompt("enter file path")
+        props.setPath(p)
+    }
     return (
-        <></>
+        <Stack sx={{m:2}} spacing={2} direction="row">
+            <Button
+                component="label"
+                varinat="contained"
+                className='bg-red-300'
+            >
+                select path
+                <input 
+                    className='hidden' 
+                    type="button" //webkitdirectory="" 
+                    onClick={selectPath}/>
+            </Button>
+            <Typography className='text-lg'>current path:{path}
+            </Typography>
+        </Stack>
     )
 }
