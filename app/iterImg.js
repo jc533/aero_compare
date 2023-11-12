3b5cf8b8d1154eac7b90ee4011d617f54575736b
JustinChen <yub612@gmail.com>
2023-11-12 14:50:08 +0800

    add lightbox

--- app/iterImg.js
+++ app/iterImg.js
@@ -1,26 +1,28 @@
 import { useContext } from 'react'
 import Image from 'next/image'
 import { Box } from '@mui/system'
+import "yet-another-react-lightbox/styles.css";
 // import { promises as fs } from 'fs'
 
 export default function IterImg({simuA,simuB,dim}){
     // const file = await fs.readFile(process.cwd() + '/app/data.json', 'utf8');
     // const data = JSON.parse(file);
     return (
-        <Box>
-        <Image
+        <Box sx={{maxWidth:700}}>
+        
+        {/* <Image
             width={400}
             height={400}
             src={simuA?`/${simuA}/${dim}.jpg`:''}
             alt="lalalala"
+            layout='responsive'
         />
-        <br/>
         <Image
             width={400}
             height={400}
             src={simuB?`/${simuB}/${dim}.jpg`:""}
             alt="lalalala"
-        />
+        /> */}
         </Box>
     )
 }
